ec697e846c0469b26b125d62e9c48e143f517c06
aube06 <65210394+aube06@users.noreply.github.com>
2021-05-12 12:05:22 +0300

    Update KeyUpdate.sh

--- KeyUpdate.sh
+++ KeyUpdate.sh
@@ -2,48 +2,48 @@
 ################################################################
 # Title:.......KeyUpdate                                       #
 # Author:......2021                                            #
-# Support:.....www.dreamosat-forum.com                         #
-# E-Mail:......admin@dreamosat-forum.com                       #
-# Date:........26.11.2018                                      #
+# Support:.....                                                #
+# E-Mail:......                                                #
+# Date:........                                                #
 # Description:.KeyUpdate                                       #
 ################################################################
 FIN="==================================================";
 echo $FIN;
 echo ".....:: LÜTFEN BEKLEYiNiZ .... PLEASE WAIT ::.....";
 ##################################################
-URL="https://raw.githubusercontent.com/aube06/SoftCam.Key_Serjoga/master/TURKvodLokal.xml";
+URL="https://raw.githubusercontent.com/aube06/SoftCam.Key_Serjoga/master";
 # Files
 TMP=`mktemp -d`
 cd ${TMP}
-[ -d /usr/lib/enigma2/python/Plugins/Extensions/TURKvod/ ] || mkdir -p /usr/lib/enigma2/python/Plugins/Extensions/TURKvod/;
+[ -d /etc/tuxbox/config/ ] || mkdir -p /etc/tuxbox/config/;
 [ -d /usr/keys ] || mkdir -p /usr/keys;
 # Github
 #agent="--header='User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/600.1.17 (KHTML, like Gecko) Version/8.0 Safari/600.1.17'"
 #crt="--no-check-certificate"
 
 #wget -q $crt $agent $URL/SoftCam.Key
-curl -s -Lbk -m 4 -m 6 ${URL}/TURKvodLokal.xml -o TURKvodLokal.xml
-# check TURKvodLokalxml
-if [ -f ${TMP}/TURKvodLokal.xml ] ; then
+curl -s -Lbk -m 4 -m 6 ${URL}/SoftCam.Key -o SoftCam.Key
+# check softcamkey
+if [ -f ${TMP}/SoftCam.Key ] ; then
   #
-  chmod 0600 ${TMP}/TURKvodLokal.xml -R;
+  chmod 0755 ${TMP}/SoftCam.Key -R;
   # check
-  if [ -f /usr/lib/enigma2/python/Plugins/Extensions/TURKvod/TURKvodLokal.xml ] ; then
-    check="/usr/lib/enigma2/python/Plugins/Extensions/TURKvod/TURKvodLokal.xml";
-  elif [ -f /usr/lib/enigma2/python/Plugins/Extensions/TURKvod/TURKvodLokal.xml ] ; then
-    check="/usr/lib/enigma2/python/Plugins/Extensions/TURKvod/TURKvodLokal.xml";
-  elif [ -f /usr/lib/enigma2/python/Plugins/Extensions/TURKvod/TURKvodLokal.xml ] ; then
-    check="/usr/lib/enigma2/python/Plugins/Extensions/TURKvod/TURKvodLokal.xml";
+  if [ -f /etc/tuxbox/config/SoftCam.Key ] ; then
+    check="/etc/tuxbox/config/SoftCam.Key";
+  elif [ -f /etc/tuxbox/config/oscam-emu/SoftCam.Key ] ; then
+    check="/etc/tuxbox/config/oscam-emu/SoftCam.Key";
+  elif [ -f /etc/tuxbox/config/oscam/SoftCam.Key ] ; then
+    check="/etc/tuxbox/config/oscam/SoftCam.Key";
   else
     echo "The SoftCam.Key file was not found.\n";
     echo "It was sent to the following folder."
-    echo ": /usr/lib/enigma2/python/Plugins/Extensions/TURKvod/"
+    echo ": /etc/tuxbox/config/"
     echo ": /usr/keys/"
-    check="/usr/lib/enigma2/python/Plugins/Extensions/TURKvod/TURKvodLokal.xml";
+    check="/etc/tuxbox/config/SoftCam.Key";
   #  exit 0;
   fi;
   # copy
-  cp -rd ${TMP}/TURKvodLokal.xml $check;
+  cp -rd ${TMP}/SoftCam.Key $check;
   wget $crt $agent --quiet -O - $URL/README.md | grep -B100 -ia 'enigma2-plugin-extensions-KeyUpdate' | sed -e 's/<[^>]*>//g' | grep -va ".deb" | grep -va ".ipk";
  # curl -Lk -s -m 4 -m 6 ${URL}/README.md | grep -B100 -ia 'enigma2-plugin-extensions-KeyUpdate' | sed -e 's/<[^>]*>//g' | grep -va ".deb" | grep -va ".ipk";
 else
